e0a3f1436b740bcc5bf4e4b68bb35059d90e57f8
Marc Sladek <marc.sladek@synventis.com>
2017-06-28 15:58:31 +0200

    add WebSearchPackage.PREDICATE_DEFAULT

--- src/main/java/com/celements/search/web/packages/WebSearchPackage.java
+++ src/main/java/com/celements/search/web/packages/WebSearchPackage.java
@@ -10,6 +10,7 @@ import org.xwiki.model.reference.DocumentReference;
 import com.celements.search.lucene.query.IQueryRestriction;
 import com.celements.search.lucene.query.LuceneDocType;
 import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import com.xpn.xwiki.doc.XWikiDocument;
 
 @ComponentRole
@@ -31,5 +32,13 @@ public interface WebSearchPackage {
 
   @NotNull
   public Optional<DocumentReference> getLinkedClassRef();
+
+  public static final Predicate<WebSearchPackage> PREDICATE_DEFAULT = new Predicate<WebSearchPackage>() {
+
+    @Override
+    public boolean apply(WebSearchPackage seachPackage) {
+      return seachPackage.isDefault();
+    }
+  };
 
 }
