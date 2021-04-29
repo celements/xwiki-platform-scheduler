45d74c1720f2b49c239a1c6269c3fe27dfde8157
Marc Sladek <marc@sladek.me>
2021-04-29 17:29:56 +0200

    Web Search: Field package extensions (#44)
    * extend FieldWebSearchPackage
    
    * add multiselect
    
    * improve pretty names
    
    * velocity interpreted

--- src/main/java/com/celements/search/web/classes/WebSearchFieldConfigClass.java
+++ src/main/java/com/celements/search/web/classes/WebSearchFieldConfigClass.java
@@ -1,5 +1,10 @@
 package com.celements.search.web.classes;
 
+import static com.google.common.collect.Lists.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import javax.annotation.concurrent.Immutable;
 import javax.inject.Singleton;
 
@@ -9,6 +14,10 @@ import org.xwiki.model.reference.ClassReference;
 import com.celements.model.classes.AbstractClassDefinition;
 import com.celements.model.classes.fields.ClassField;
 import com.celements.model.classes.fields.StringField;
+import com.celements.model.classes.fields.list.EnumListField;
+import com.celements.model.classes.fields.list.single.EnumSingleListField;
+import com.celements.model.classes.fields.number.FloatField;
+import com.celements.search.lucene.query.QueryRestrictionGroup.Type;
 
 @Immutable
 @Singleton
@@ -24,6 +33,21 @@ public class WebSearchFieldConfigClass extends AbstractClassDefinition implement
   public static final ClassField<String> FIELD_NAME = new StringField.Builder(
       CLASS_REF, "fieldName").build();
 
+  public static final ClassField<Type> FIELD_OPERATOR = new EnumSingleListField.Builder<>(
+      CLASS_REF, "operator", Type.class).prettyName("Operator (default: OR)")
+          .values(reverse(Arrays.asList(Type.values())))
+          .build();
+
+  public static final ClassField<List<SearchMode>> FIELD_SEARCH_MODE = new EnumListField.Builder<>(
+      CLASS_REF, "searchMode", SearchMode.class).prettyName("Search Mode (default: all)")
+          .multiSelect(true).build();
+
+  public static final ClassField<Float> FIELD_BOOST = new FloatField.Builder(
+      CLASS_REF, "boost").prettyName("Boost (default: 1.0)").build();
+
+  public static final ClassField<String> FIELD_VALUE = new StringField.Builder(
+      CLASS_REF, "value").prettyName("Value (Velocity, optional)").build();
+
   public WebSearchFieldConfigClass() {
     super(CLASS_REF);
   }
@@ -31,6 +55,10 @@ public class WebSearchFieldConfigClass extends AbstractClassDefinition implement
   @Override
   public boolean isInternalMapping() {
     return false;
+  }
+
+  public enum SearchMode {
+    TOKENIZED, EXACT;
   }
 
 }
--- src/main/java/com/celements/search/web/packages/FieldWebSearchPackage.java
+++ src/main/java/com/celements/search/web/packages/FieldWebSearchPackage.java
@@ -1,16 +1,27 @@
 package com.celements.search.web.packages;
 
 import static com.celements.search.lucene.LuceneUtils.*;
+import static com.celements.search.web.classes.WebSearchFieldConfigClass.*;
+import static java.util.stream.Collectors.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.ClassReference;
+import org.xwiki.velocity.XWikiVelocityException;
 
+import com.celements.model.field.FieldAccessor;
+import com.celements.model.field.XObjectFieldAccessor;
 import com.celements.model.object.xwiki.XWikiObjectFetcher;
 import com.celements.search.lucene.ILuceneSearchService;
 import com.celements.search.lucene.query.IQueryRestriction;
@@ -18,17 +29,30 @@ import com.celements.search.lucene.query.LuceneDocType;
 import com.celements.search.lucene.query.QueryRestrictionGroup;
 import com.celements.search.lucene.query.QueryRestrictionGroup.Type;
 import com.celements.search.web.classes.WebSearchFieldConfigClass;
+import com.celements.search.web.classes.WebSearchFieldConfigClass.SearchMode;
+import com.celements.velocity.VelocityService;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+import one.util.streamex.EntryStream;
 
 @ThreadSafe
 @Singleton
 @Component(FieldWebSearchPackage.NAME)
 public class FieldWebSearchPackage implements WebSearchPackage {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(FieldWebSearchPackage.class);
+
   public static final String NAME = "field";
 
   @Requirement
   private ILuceneSearchService searchService;
+
+  @Requirement(XObjectFieldAccessor.NAME)
+  private FieldAccessor<BaseObject> xFieldAccess;
+
+  @Requirement
+  private VelocityService velocityService;
 
   @Override
   public String getName() {
@@ -52,13 +76,50 @@ public class FieldWebSearchPackage implements WebSearchPackage {
 
   @Override
   public IQueryRestriction getQueryRestriction(XWikiDocument cfgDoc, String searchTerm) {
-    QueryRestrictionGroup grp = searchService.createRestrictionGroup(Type.OR);
-    getConfigObjFetcher(cfgDoc).fetchField(WebSearchFieldConfigClass.FIELD_NAME).stream()
-        .flatMap(fieldName -> Stream.of(
-            searchService.createRestriction(fieldName, searchTerm, true).setBoost(1),
-            searchService.createRestriction(fieldName, exactify(searchTerm), false).setBoost(2)))
-        .forEach(grp::add);
+    Map<Type, List<BaseObject>> groupedObjs = getConfigObjFetcher(cfgDoc).stream().collect(
+        groupingBy(obj -> xFieldAccess.get(obj, FIELD_OPERATOR).orElse(Type.OR)));
+    return createRestrictionGroup(Type.AND, EntryStream.of(groupedObjs)
+        .mapValues(objs -> objs.stream().map(obj -> asRestriction(obj, searchTerm)))
+        .mapKeyValue(this::createRestrictionGroup));
+  }
+
+  private QueryRestrictionGroup createRestrictionGroup(Type type,
+      Stream<? extends IQueryRestriction> restr) {
+    QueryRestrictionGroup grp = searchService.createRestrictionGroup(type);
+    restr.forEach(grp::add);
     return grp;
+  }
+
+  private IQueryRestriction asRestriction(BaseObject obj, String searchTerm) {
+    String field = xFieldAccess.get(obj, FIELD_NAME).orElse("");
+    String value = xFieldAccess.get(obj, FIELD_VALUE)
+        .map(this::evaluateVelocityText).orElse(searchTerm);
+    float boost = xFieldAccess.get(obj, FIELD_BOOST).orElse(1f);
+    return createRestrictionGroup(Type.OR, xFieldAccess.get(obj, FIELD_SEARCH_MODE)
+        .orElseGet(() -> Arrays.asList(SearchMode.values()))
+        .stream()
+        .map(mode -> createRestriction(mode, field, value, boost)));
+  }
+
+  private IQueryRestriction createRestriction(SearchMode mode,
+      String field, String value, float boost) {
+    switch (mode) {
+      case TOKENIZED:
+        return searchService.createRestriction(field, value, true).setBoost(boost);
+      case EXACT:
+        return searchService.createRestriction(field, exactify(value), false).setBoost(boost * 2);
+      default:
+        throw new IllegalArgumentException(Objects.toString(mode));
+    }
+  }
+
+  private String evaluateVelocityText(String text) {
+    try {
+      return velocityService.evaluateVelocityText(text);
+    } catch (XWikiVelocityException exc) {
+      LOGGER.info("evaluateVelocityText: failed", exc);
+      return text;
+    }
   }
 
   @Override
