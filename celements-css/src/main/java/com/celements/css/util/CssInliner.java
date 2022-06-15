3a4b96fd70dd606dd6bb25fa5df2ce32e083c185
pichlerf <fabian.pichler@synventis.com>
2022-06-15 12:54:49 +0200

    introduce CssInliner

---
+++ celements-css/src/main/java/com/celements/css/util/CssInliner.java
@@ -0,0 +1,30 @@
+package com.celements.css.util;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+
+import org.xwiki.component.annotation.ComponentRole;
+
+import com.celements.css.exception.CssInlineException;
+
+@ComponentRole
+public interface CssInliner {
+
+  @NotNull
+  String inline(@NotNull String html, @NotNull List<String> cssList) throws CssInlineException;
+
+  @NotNull
+  String inline(@NotNull String html, @NotNull List<String> cssList,
+      @Nullable Map<String, String> configs) throws CssInlineException;
+
+  @NotNull
+  String inline(@NotNull String html, @NotNull String css) throws CssInlineException;
+
+  @NotNull
+  String inline(@NotNull String html, @NotNull String css, @Nullable Map<String, String> configs)
+      throws CssInlineException;
+
+}
