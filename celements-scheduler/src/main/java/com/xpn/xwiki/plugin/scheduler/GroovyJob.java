d77f21bd3dd03c2e746f8a3257baf604495ecc7a
Chantal Pichler <chantal.pichler@synventis.com>
2023-05-17 10:45:39 +0200

    added javadoc explanation

--- celements-scheduler/src/main/java/com/xpn/xwiki/plugin/scheduler/GroovyJob.java
+++ celements-scheduler/src/main/java/com/xpn/xwiki/plugin/scheduler/GroovyJob.java
@@ -1,5 +1,10 @@
 package com.xpn.xwiki.plugin.scheduler;
 
+/**
+ * This class is only needed for existing DB references to still work after renaming the scheduler
+ * package. Use com.celements.scheduler.job.GroovyJob for new work.
+ */
+
 @Deprecated
 public class GroovyJob extends com.celements.scheduler.job.GroovyJob {
 
