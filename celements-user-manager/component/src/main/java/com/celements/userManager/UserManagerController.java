fceaecbc8f1a1367805e47e60bac1adbf51ffde5
Chantal Pichler <chantal.pichler@synventis.com>
2023-06-09 16:09:04 +0200

    render UserAdmin Layout in /api/celAdmin

--- celements-user-manager/component/src/main/java/com/celements/userManager/UserManagerController.java
+++ celements-user-manager/component/src/main/java/com/celements/userManager/UserManagerController.java
@@ -2,35 +2,35 @@ package com.celements.userManager;
 
 import javax.inject.Inject;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.xwiki.context.Execution;
+import org.xwiki.model.reference.SpaceReference;
 
+import com.celements.model.context.ModelContext;
+import com.celements.model.reference.RefBuilder;
 import com.celements.pagelayout.LayoutServiceRole;
-import com.xpn.xwiki.XWikiContext;
 
 @Controller
 public class UserManagerController {
 
-  @Inject
-  private BeanFactory beanFactory;
+  private final ModelContext context;
 
   private final LayoutServiceRole layoutService;
 
   @Inject
-  public UserManagerController(BeanFactory beanFactory, LayoutServiceRole layoutService) {
-    this.beanFactory = beanFactory;
+  public UserManagerController(
+      LayoutServiceRole layoutService,
+      ModelContext context) {
     this.layoutService = layoutService;
+    this.context = context;
   }
 
   @GetMapping("/celAdmin")
   @ResponseBody
-  public XWikiContext celAdmin() {
-    return (XWikiContext) beanFactory
-        .getBean(Execution.class)
-        .getContext()
-        .getProperty(XWikiContext.EXECUTIONCONTEXT_KEY);
+  public String celAdmin() {
+    SpaceReference userAdminSpaceRef = RefBuilder.from(context.getWikiRef()).space("UserAdmin")
+        .build(SpaceReference.class);
+    return layoutService.renderPageLayout(userAdminSpaceRef);
   }
 }
