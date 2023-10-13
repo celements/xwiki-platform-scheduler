29b7ef4322aca84347ba118b840caf58dc98b14c
Marc Sladek <marc@sladek.dev>
2023-10-13 12:35:42 +0200

    TagDto

--- celements-tag/src/main/java/com/celements/tag/controller/CelTagController.java
+++ celements-tag/src/main/java/com/celements/tag/controller/CelTagController.java
@@ -2,11 +2,11 @@ package com.celements.tag.controller;
 
 import static java.util.stream.Collectors.*;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.annotation.concurrent.Immutable;
 import javax.inject.Inject;
 
 import org.springframework.http.ResponseEntity;
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.celements.tag.CelTag;
 import com.celements.tag.CelTagService;
+
+import one.util.streamex.EntryStream;
 
 @RestController
 @RequestMapping("/celtags")
@@ -30,8 +32,11 @@ public class CelTagController {
   }
 
   @GetMapping
-  public Map<String, Collection<CelTag>> getTags() {
-    return tagService.getTagsByType().asMap();
+  public Map<String, List<TagDto>> getTags() {
+    return EntryStream.of(tagService.getTagsByType().entries().stream())
+        .filterValues(CelTag::isRoot)
+        .mapValues(TagDto::new)
+        .grouping();
   }
 
   @GetMapping("/types")
@@ -39,21 +44,38 @@ public class CelTagController {
     return tagService.getTagsByType().keySet();
   }
 
-  @GetMapping("/types/{type}")
-  public List<CelTag> getTagsByType(
+  @GetMapping("/{type}")
+  public List<TagDto> getTagsByType(
       @PathVariable String type) {
     return tagService.getTagsByType().get(type).stream()
+        .filter(CelTag::isRoot)
         .sorted(CelTag.CMP_ORDER)
+        .map(TagDto::new)
         .collect(toList());
   }
 
-  @GetMapping("/types/{type}/names/{name}")
-  public ResponseEntity<CelTag> getTagByName(
+  @GetMapping("/{type}/{name}")
+  public ResponseEntity<TagDto> getTagByName(
       @PathVariable String type,
       @PathVariable String name) {
     return tagService.getTag(type, name)
+        .map(TagDto::new)
         .map(ResponseEntity::ok)
         .orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @Immutable
+  public class TagDto {
+
+    public final String name;
+    public final String type;
+    public final List<TagDto> children;
+
+    public TagDto(CelTag tag) {
+      name = tag.getName();
+      type = tag.getType();
+      children = tag.getChildren().map(TagDto::new).toImmutableList(); // TODO sort?
+    }
   }
 
 }
