3b381b57afab60c9c6c88daa63ab7599f371b64b
Chantal Pichler <chantal.pichler@synventis.com>
2024-02-09 17:50:58 +0100

    add first version of sendActivationMail.mjs

---
+++ celements-user-manager/web-module/src/main/webapp/resources/celJS/userAdmin/sendActivationMail.mjs
@@ -0,0 +1,27 @@
+/* Request über Fetch API absetzen */
+async function sendActivationMail(url) {
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('response status: ' + response.status + ' ' + response.statusText + ' for url: ' + url);
+      }
+      const data = await response.json();
+      alert(data.message);
+  } catch (error) { 
+    console.error('Error during fetch operation:', error);
+  }
+}
+
+
+/* normale Aktion des a-Tags unterdrücken und einen Alert anzeigen */
+function stopDefaultAction(event) {
+    event.preventDefault();
+    sendActivationMail(event.currentTarget.href);
+}
+
+/* EventListener für a Tags registrieren */
+document.querySelectorAll("a.sendMailAction").forEach(a => {
+    a.addEventListener('click', stopDefaultAction);
+});
+
+
