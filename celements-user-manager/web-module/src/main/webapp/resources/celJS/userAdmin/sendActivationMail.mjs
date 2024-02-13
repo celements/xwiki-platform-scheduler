7d7c3fd94885c010282e4b828a75653d5c423fdb
Chantal Pichler <chantal.pichler@synventis.com>
2024-02-13 18:14:52 +0100

    refactor to class

--- celements-user-manager/web-module/src/main/webapp/resources/celJS/userAdmin/sendActivationMail.mjs
+++ celements-user-manager/web-module/src/main/webapp/resources/celJS/userAdmin/sendActivationMail.mjs
@@ -1,4 +1,52 @@
-/* Request über Fetch API absetzen */
+class SendMailButton {
+  #htmlElemA;
+  #iconElem;
+  constructor(htmlElemA) {
+    this.#htmlElemA = htmlElemA;
+    this.#iconElem = this.#htmlElemA.querySelector(".icon");
+  }
+
+  register() {
+    console.log('registering event listener for ' + this.#htmlElemA);
+    this.#htmlElemA.addEventListener('click', (ev) => this.#stopDefaultAction(ev));
+  }
+
+  #stopDefaultAction(event) {
+    console.log('stopDefaultAction');
+    event.preventDefault();
+    this.#iconElem.classList.add("fa-spinner", "fa-spin");
+    this.#sendActivationMail(this.#htmlElemA.href);
+  }
+
+  async #sendActivationMail(url) {
+    console.log('sendActivationMail with url: ' + url);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('response status: ' + response.status + ' ' + response.statusText + ' for url: ' + url);
+      }
+      const data = await response.json();
+      this.#showOutput(data.message);
+    } catch (error) { 
+      console.error('Error during fetch operation:', error);
+      this.#showOutput("Es ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.\n" + error.message);
+    } finally {
+      this.#iconElem.classList.remove("fa-spinner", "fa-spin");
+    }
+  }
+
+  #showOutput(message) {
+    alert(message);
+  }
+}
+
+document.querySelectorAll("a.sendMailAction").forEach((a) => new SendMailButton(a).register());
+
+
+
+
+/*___________________________________________________________
+
 async function sendActivationMail(event, url) {
   try {
       const response = await fetch(url);
@@ -15,14 +63,13 @@ async function sendActivationMail(event, url) {
   }
 }
 
-/* normale Aktion des a-Tags unterdrücken */
 function stopDefaultAction(event) {
     event.preventDefault();
     event.target.classList.add("fa-spinner", "fa-spin");
     sendActivationMail(event, event.currentTarget.href);
 }
 
-/* EventListener für a Tags registrieren */
 document.querySelectorAll("a.sendMailAction").forEach(a => {
     a.addEventListener('click', stopDefaultAction);
 });
+*/
