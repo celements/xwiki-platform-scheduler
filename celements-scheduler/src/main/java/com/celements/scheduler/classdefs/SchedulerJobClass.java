da963cf47c7c063a5c6d634e9029cacd5e581920
Chantal Pichler <chantal.pichler@synventis.com>
2024-07-17 12:02:24 +0200

    space fields better

--- celements-scheduler/src/main/java/com/celements/scheduler/classdefs/SchedulerJobClass.java
+++ celements-scheduler/src/main/java/com/celements/scheduler/classdefs/SchedulerJobClass.java
@@ -10,31 +10,69 @@ import com.celements.model.classes.fields.StringField;
 import com.xpn.xwiki.XWikiConstant;
 
 @Component(SchedulerJobClass.CLASS_DEF_HINT)
-public class SchedulerJobClass extends AbstractClassDefinition implements SchedulerJobClassDefinition {
+public class SchedulerJobClass extends AbstractClassDefinition
+    implements SchedulerJobClassDefinition {
 
   public static final String DOC_NAME = "SchedulerJobClass";
   public static final String XWIKI_SPACE = XWikiConstant.XWIKI_SPACE;
   public static final String CLASS_DEF_HINT = XWIKI_SPACE + "." + DOC_NAME;
   public static final ClassReference CLASS_REF = new ClassReference(XWIKI_SPACE, DOC_NAME);
 
-  public static final ClassField<String> FIELD_JOB_NAME = new StringField.Builder(CLASS_REF,
-      "jobName").prettyName("Job Name").size(60).build();
+  public static final ClassField<String> FIELD_JOB_NAME = new StringField.Builder(
+      CLASS_REF, "jobName")
+          .prettyName("Job Name")
+          .size(60)
+          .build();
+
   public static final ClassField<String> FIELD_JOB_DESCRIPTION = new LargeStringField.Builder(
-      CLASS_REF, "jobDescription").rows(10).prettyName("Job Description").size(45).build();
-  public static final ClassField<String> FIELD_JOB_CLASS = new StringField.Builder(CLASS_REF,
-      "jobClass").prettyName("Job Class").size(60).build();
-  public static final ClassField<String> FIELD_STATUS = new StringField.Builder(CLASS_REF,
-      "status").prettyName("Status").size(30).build();
-  public static final ClassField<String> FIELD_CRON = new StringField.Builder(CLASS_REF, "cron")
-      .prettyName("Cron Expression").size(30).build();
-  public static final ClassField<String> FIELD_SCRIPT = new LargeStringField.Builder(CLASS_REF,
-      "script").rows(10).prettyName("Job Script").size(60).build();
-  public static final ClassField<String> FIELD_CONTEXT_USER = new StringField.Builder(CLASS_REF,
-      "contextUser").prettyName("Job execution context user").size(30).build();
-  public static final ClassField<String> FIELC_CONTEXT_LANG = new StringField.Builder(CLASS_REF,
-      "contextLang").prettyName("Job execution context lang").size(30).build();
-  public static final ClassField<String> FIELD_CONTEXT_DATABASE = new StringField.Builder(CLASS_REF,
-      "contextDatabase").prettyName("Job execution context database").size(30).build();
+      CLASS_REF, "jobDescription")
+          .rows(10)
+          .prettyName("Job Description")
+          .size(45)
+          .build();
+
+  public static final ClassField<String> FIELD_JOB_CLASS = new StringField.Builder(
+      CLASS_REF, "jobClass")
+          .prettyName("Job Class")
+          .size(60)
+          .build();
+
+  public static final ClassField<String> FIELD_STATUS = new StringField.Builder(
+      CLASS_REF, "status")
+          .prettyName("Status")
+          .size(30)
+          .build();
+
+  public static final ClassField<String> FIELD_CRON = new StringField.Builder(
+      CLASS_REF, "cron")
+          .prettyName("Cron Expression")
+          .size(30)
+          .build();
+
+  public static final ClassField<String> FIELD_SCRIPT = new LargeStringField.Builder(
+      CLASS_REF, "script")
+          .rows(10)
+          .prettyName("Job Script")
+          .size(60)
+          .build();
+
+  public static final ClassField<String> FIELD_CONTEXT_USER = new StringField.Builder(
+      CLASS_REF, "contextUser")
+          .prettyName("Job execution context user")
+          .size(30)
+          .build();
+
+  public static final ClassField<String> FIELC_CONTEXT_LANG = new StringField.Builder(
+      CLASS_REF, "contextLang")
+          .prettyName("Job execution context lang")
+          .size(30)
+          .build();
+
+  public static final ClassField<String> FIELD_CONTEXT_DATABASE = new StringField.Builder(
+      CLASS_REF, "contextDatabase")
+          .prettyName("Job execution context database")
+          .size(30)
+          .build();
 
   public SchedulerJobClass() {
     super(CLASS_REF);
