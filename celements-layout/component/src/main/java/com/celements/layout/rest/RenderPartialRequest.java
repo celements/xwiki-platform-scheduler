1b600a24d6edb9d1c4fef6573ed223c8a2feb51f
Fabian Pichler <fabian.pichler@synventis.com>
2025-01-19 04:35:19 +0100

    extract layout service from celements-core

---
+++ celements-layout/component/src/main/java/com/celements/layout/rest/RenderPartialRequest.java
@@ -0,0 +1,10 @@
+package com.celements.layout.rest;
+
+public class RenderPartialRequest {
+
+  public String contextDocSpace;
+  public String contextDocName;
+  public String layoutSpace;
+  public String startNodeName;
+
+}
